567da40274b0d61c7ff20ab59be9724c2ff6ed2b
Raj Varsani <varasaniraj5@gmail.com>
2024-08-28 00:35:59 +0530

    feat: add users data fetching service

---
+++ services/users.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { ApplicationDoc } from "../arsenal/models/application";
+import SecurityConfiguration from "../arsenal/models/security-conf";
+import { SecurityConfigType } from "../arsenal/types/security-conf";
+import { populateData } from "./population";
+
+export const fetchUsersData = async (app: ApplicationDoc) => {
+  try {
+    const tokensConfig = await SecurityConfiguration.findOne({
+      app,
+      configType: SecurityConfigType.USER_DATA,
+    });
+
+    const API_URL = app.baseUrl + tokensConfig?.rules?.endpoint;
+    const headers = populateData(
+      tokensConfig?.rules?.headers,
+      // *This will be based on app
+      process.env.PHALANX_SHARED_SECRET,
+      {},
+      {}
+    );
+
+    const { data } = await axios.get(API_URL, {
+      headers,
+    });
+
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch user data");
+  }
+};
