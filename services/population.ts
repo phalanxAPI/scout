d830bb8309fa3ce87ca08996417d6a8ea71604cd
Raj Varsani <varasaniraj5@gmail.com>
2024-08-28 21:32:53 +0530

    feat: support synthetic test cases gen via regex exp

--- services/population.ts
+++ services/population.ts
@@ -1,3 +1,5 @@
+import RandExp from "randexp";
+
 type TokenData = Record<string, string>;
 type UserData = Record<string, Record<string, any>>;
 
@@ -16,18 +18,25 @@ const populateData = (
 
   const processString = (value: string): string => {
     return value.replace(/{{(.*?)}}/g, (_, p1: string) => {
-      const parts = p1.split(".");
-      let currentData: any = null;
-      if (parts[0] === "SHARED_SECRET") {
-        return sharedSecret;
-      } else if (userData[parts[0]]) {
-        currentData = userData[parts[0]];
-        for (let i = 1; i < parts.length; i++) {
-          currentData = currentData[parts[i]];
-          if (currentData === undefined) break;
+      // Splitting the placeholder based on ":" to check for "regex" or other keywords
+      const colonParts = p1.split(":");
+      if (colonParts[0] === "regex") {
+        return new RandExp(colonParts[1]).gen();
+      } else {
+        // Handling for shared secrets or user data
+        const dotParts = p1.split(".");
+        let currentData: any = null;
+        if (dotParts[0] === "SHARED_SECRET") {
+          return sharedSecret;
+        } else if (userData[dotParts[0]]) {
+          currentData = userData[dotParts[0]];
+          for (let i = 1; i < dotParts.length; i++) {
+            currentData = currentData[dotParts[i]];
+            if (currentData === undefined) break;
+          }
         }
+        return currentData !== null ? currentData : `{{${p1}}}`;
       }
-      return currentData !== null ? currentData : `{{${p1}}}`;
     });
   };
 
