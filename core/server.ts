fc8e211c763aec2d33b83d55297ba82f62fe4b85
Raj Varsani <varasaniraj5@gmail.com>
2024-09-09 00:56:23 +0530

    refactor: update scout service import

--- core/server.ts
+++ core/server.ts
@@ -5,15 +5,14 @@ import { ScoutService } from "../types/proto";
 
 const PORT = process.env.PORT || 9001;
 
-// const packageDef = loadSync("arsenal/proto/scout.proto", {});
-const packageDef = loadSync("arsenal/proto/sysmon.lighthouse.proto", {});
+const packageDef = loadSync("arsenal/proto/scout.proto", {});
 const gRPCObject = gRPC.loadPackageDefinition(packageDef);
 
 const phalanxPackage = gRPCObject.phalanx as GrpcObject;
 const arsenalPackage = phalanxPackage.arsenal as GrpcObject;
-const scoutPackage = arsenalPackage.sysmon as GrpcObject;
+const scoutPackage = arsenalPackage.scout as GrpcObject;
 
-const scoutConstructor = scoutPackage.SysmonService as ServiceClientConstructor;
+const scoutConstructor = scoutPackage.ScoutService as ServiceClientConstructor;
 const scoutService = scoutConstructor.service;
 
 const server = new gRPC.Server();
